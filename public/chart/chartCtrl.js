35e82844bf4f29020cd32a36ba73675475730e58
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-14 15:54:19 -0700

    syntax

--- public/chart/chartCtrl.js
+++ public/chart/chartCtrl.js
@@ -1,33 +1,32 @@
 angular.module('chart.ctrl', [])
-  
-.controller('chartCtrl', ['$scope','tweetFactory','$http', '$rootScope', function ($scope, tweetFactory, $http, $rootScope) {
+
+.controller('chartCtrl', ['$scope', 'tweetFactory', '$http', '$rootScope', function($scope, tweetFactory, $http, $rootScope) {
   $scope.showChart = false;
   // $scope.series = ['test'];
   // $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
   // $scope.data = [300, 500, 100];
   console.log('chartCTRL');
   $scope.options = {
-        responsive: true,
-        maintainAspectRatio: true,
-        barDatasetSpacing: 1,
-        barShowStroke: true,
-        barStrokeWidth : 2,
-        barValueSpacing : 5
-    };
+    responsive: true,
+    maintainAspectRatio: true,
+    barDatasetSpacing: 1,
+    barShowStroke: true,
+    barStrokeWidth: 2,
+    barValueSpacing: 5
+  };
 
-  $rootScope.$watch('hashTagKeys', function(){
+  $rootScope.$watch('hashTagKeys', function() {
     console.info('hashtagkeys changed');
     $scope.labels = $rootScope.hashTagKeys;
     $scope.data = $rootScope.hashTagValues;
     $scope.showChart = true;
   });
-  
 
 
-  $scope.tweets=[];
-  $scope.test = function(tweet){
+
+  $scope.tweets = [];
+  $scope.test = function(tweet) {
     console.log(tweet);
   };
 
-},
-]);
+}, ]);
--- public/feed/feedCtrl.js
+++ public/feed/feedCtrl.js
@@ -1,17 +1,16 @@
 angular.module('feed.ctrl', ['tweet.factory'])
-  
-.controller('feedCtrl', ['$scope','tweetFactory','$http', '$rootScope', function ($scope, tweetFactory, $http, $rootScope) {
+
+.controller('feedCtrl', ['$scope', 'tweetFactory', '$http', '$rootScope', function($scope, tweetFactory, $http, $rootScope) {
   console.log('feedCTRL');
-  
-  $scope.tweets=[];
-  tweetFactory.stream.on('tweets', function(data){
+
+  $scope.tweets = [];
+  tweetFactory.stream.on('tweets', function(data) {
     tweetFactory.handleTweet(data[0]);
     $scope.tweets = data.concat($scope.tweets);
   });
-  $scope.changeStream = function(query){
-    $http.get('//localhost:3000/twitterStream/' + query).then(function (results){
+  $scope.changeStream = function(query) {
+    $http.get('//localhost:3000/twitterStream/' + query).then(function(results) {
       console.log("success");
     });
   };
-},
-]);
+}, ]);
